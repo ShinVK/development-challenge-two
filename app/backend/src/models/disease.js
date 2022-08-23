d46e4a73dd69030517a02064170e2497cce83046
shinvk <victor.kamiguchi@gmail.com>
2022-08-23 15:56:31 -0300

    feat(fix models sequelize): fix model sequelize

--- app/backend/src/models/disease.js
+++ app/backend/src/models/disease.js
@@ -1,23 +1,7 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Disease extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Disease.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Disease',
+  const Disease = sequelize.define('Disease', {
+    name: DataTypes.STRING,
   });
+  
   return Disease;
 };
--- app/backend/src/models/personaldata.js
+++ app/backend/src/models/personaldata.js
@@ -1,24 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class PersonalData extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  PersonalData.init({
+  const PersonalData = sequelize.define('PersonalData', {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     birthDate: DataTypes.DATE,
     city: DataTypes.STRING,
-    state: DataTypes.STRING
+    state: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'PersonalData',
